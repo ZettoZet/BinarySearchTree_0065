e7282a60f788fb3922f7f5fa54d0dd5988dab3f6
Pillow <m.farid.ft20@mail.umy.ac.id>
2025-06-26 16:56:50 +0700

    Adding Node Class
    Adding Node class that contain variable, pointer, and constructor to ensure the pointer doesn't point anywhere as soon as the program start

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -1,5 +1,20 @@
 #include <iostream>
 using namespace std;
+class Node
+{
+public:
+    int info;
+    Node *leftchild;
+    Node *rightchild;
+
+    // constructor for the node class
+    Node()
+    {
+        leftchild = nullptr;  // Initialize left child to nullptr
+        rightchild = nullptr; // Initialize right child to nullptr
+    }
+};
+
 
 
 int main()
